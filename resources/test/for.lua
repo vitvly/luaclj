bf91edd291784b96e8a4223aeabd857ff5cff895
Vitaliy Vlasov <siphiuel@gmail.com>
2016-11-02 18:08:24 +0200

    Add lua file with for and table statements

---
+++ resources/test/for.lua
@@ -0,0 +1,15 @@
+sum = 0
+for j = 1, 100 do
+  sum = sum + j
+end
+
+t = {1, 10, 30}
+t['a'] = 9
+key = 22
+t1 = {a=2, b=3, c=4}
+t2 = {[key]=10, ['a']=20, ['b']=30, ['c']=40}
+
+for i,v in ipairs(t2) do
+  sum = sum + v
+end
+return sum
